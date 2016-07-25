a88d40e65e9468fd0773ae472515f25068fbc110
Kent Chang <kent12342004@gmail.com>
2016-07-25 15:39:15 +0800

    Modify the naming rule

--- 123.py
+++ 123.py
@@ -1,9 +1,58 @@
 #!/usr/bin python
 
-import sys, re
+import sys, re, random
+from collections import OrderedDict
+
+
+def getActSize(size):
+	Sum = 1
+	size = size-1
+	while size>0:
+		Sum *= 2
+		size -= 1
+	return Sum
+
+def getNewCode(data):
+
+	newCode = ''
+	exist = 0
+	compare = 0
+	asciiString = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
+	while True:
+		#for x in reversed(codeTable):
+		newCode += random.choice(asciiString)
+		#print "X:%s"%x
+		#print "NEW:%s"%newCode
+		#for y in data.keys():
+		#print newCode
+		if newCode in data.keys():
+			#print "Y:%s"%y
+			#if str(newCode) == str(y):
+				#print data.keys()
+				#print "KEY:%s"%y
+			exist += 1
+			#newCode += str(x)
+			#break
+		compare += 1
+		#print compare, exist
+		if compare>exist: 
+			break
+		
+	data.update({newCode:{}})
+	return newCode
+
 
 def count_length(l):
 	return str(bin(l)[2:])
+
+'''
+Count the toggle
+	a and b are now's value and last value
+1. extend a or b if necessary
+2. do xor to each bit with a and b
+	sum +=1 when meet 1
+3. return sum
+'''
 def toggle_count(a, b):
 	extend = 0
 	if len(a)!=len(b):
@@ -18,15 +67,22 @@ def toggle_count(a, b):
 		s = int(x)^int(y)
 		if s==1:
 			Sum+=1
-
 	return Sum
+
+#def parse_vcd(vcd_file, start_time, end_time, csv):
 def parse_vcd(vcd_file, start_time, end_time):
 	re_time = re.compile(r"^#(\d+)")				# re obj of '#(time)'
 	re_1b_val = re.compile(r"^([01zxZX])(.+)")		# re obj of e.g '0%'
 	re_Nb_val = re.compile(r"^[br](\S+)\s+(.+)")	# re obj of e.g 'b0 @', 'b111 #'
 	re_info = re.compile(r"^(\s+\S+)+")			# re obj of e.g '	Jun, ...'
 	re_t_val = re.compile(r"^(\d+)\s+(.+)")
+	re_init = re.compile(r"^[zxZX](.+)")
+	re_len = re.compile("^\[\d+\:\d+\]")
 
+	codeTable = ['!','"','#','$','%','&',"'",'(',')','*','+',',','-','.','/','0','1','2','3','4','5','6','7','8','9',
+	':',';','<','=','>','?','@','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+	'[','\\',']','^','_','`','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+	'{','|','}','~']
 	data = {}		# var structure values
 	hier = []		# scope hierachy
 	time = 0		# each '#(time)'
@@ -35,10 +91,11 @@ def parse_vcd(vcd_file, start_time, end_time):
 	write_file = [] # write file list
 	checkVars = False
 	startWrite = False
-	safeList = []	# clk
+	safeList = []	# store clk
 	changeList = []	# extend to 32
 	c = 0
-
+	sortDict = {}
+	
 	fh = open(vcd_file, 'r')
 	print "Open File!"
 	print "Start: %s, End: %s"%(start_time, end_time)
@@ -47,7 +104,7 @@ def parse_vcd(vcd_file, start_time, end_time):
 		line = fh.readline()
 		if line =='':		# EOF
 			break
-		line = line.strip()
+		line = line.strip()	# discard \s of head and tail
 
 		if "$date" in line:
 			write_file.append(line)
@@ -78,6 +135,11 @@ def parse_vcd(vcd_file, start_time, end_time):
 			vSize = ls[2]				# size		#	$var wire 4 ( addr [3:0] $end
 			vCode = ls[3]				# code
 			vName = " ".join(ls[4:-1])	# name
+			if ls[5] != '$end':
+				vLength = ''.join(ls[5:-1])
+			else:
+				vLength = '1'
+			shortName = ls[4]
 			vPath = '.'.join(hier)
 			vFullName = vPath + vName
 			if vCode not in data:
@@ -86,6 +148,8 @@ def parse_vcd(vcd_file, start_time, end_time):
 					data[vCode]['nets'] = []
 				var_struct = {
 					'type': vType,
+					'shortName': shortName,
+					'length': vLength,
 					'name': vName,
 					'size': vSize,
 					'hier': vPath,
@@ -93,22 +157,28 @@ def parse_vcd(vcd_file, start_time, end_time):
 				if var_struct not in data[vCode]['nets']:
 					data[vCode]['nets'].append(var_struct)
 			if 'clk' in str(vName) or 'reset' in str(vName):	# save clk and reset
-				temp = "$var %s\t  %s %s\t%s  $end"%(vType, vSize, vCode, vName)
+				#temp = "$var %s\t  %s %s\t%s  $end"%(vType, vSize, vCode, vName)
 				safeList.append(vCode)
+			
+			'''
 			elif str(vSize)!='32':				# change size (not 32bits)
 				changeList.append(vCode)
 				vName = ls[4]+" [31:0]"
 				temp = "$var %s\t  32 %s\t%s  $end"%(vType, vCode, str(vName))
+			
 			else:
-				temp = "$var %s\t  %s %s\t%s  $end"%(vType, vSize, vCode, str(vName))
+			'''
+			temp = "$var %s\t  %s %s\t%s  $end"%(vType, vSize, vCode, str(vName))
 			write_file.append(temp)
+
 		elif line.startswith('#'):
 			re_time_match = re_time.match(line)
 			time = re_time_match.group(1)		# recording #(time)
 			if (int(time)>int(end_time)):
 				break
 			if time=='0':
-				write_file.append(line)
+				write_file.append(line)			# needs to record #0
+
 		elif line.startswith(('0', '1', 'x', 'z', 'b', 'r','X', 'Z')):
 			if line.startswith(('0', '1', 'x', 'z', 'X', 'Z')):
 				t = re_t_val.match(line)
@@ -129,6 +199,7 @@ def parse_vcd(vcd_file, start_time, end_time):
 					write_file.append(p)
 				else:
 					write_file.append(line)
+
 			if (code in data):					# record time and value (once a time)
 				if 'tv' not in data[code]:
 					data[code]['tv'] = []
@@ -136,6 +207,7 @@ def parse_vcd(vcd_file, start_time, end_time):
 					data[code]['index'] = -1
 				if 'count' not in data[code]:
 					data[code]['count'] = 0
+				data[code]['replace'] = False
 				index = data[code]['index']
 				if index>=0:
 					if data[code]['tv'][index][0]==time:
@@ -148,22 +220,26 @@ def parse_vcd(vcd_file, start_time, end_time):
 					data[code]['tv'].append([time,value])
 					data[code]['index'] += 1
 					index = data[code]['index']
+
 		elif line.startswith("$end"):
 			write_file.append(line)
 			if checkVars:
 				checkVars = False
 				startWrite = True
-		else:
+
+		else:						# sth else
 			s = "\t%s"%line
 			write_file.append(s)
 
 	if startWrite:
-		write_file.append("#%s"%start_time)
+		#write_file.append("#%s"%start_time)
+		write_file.append("#1")
 		t = 0
 		clk = ""
 		for code in data:
 			if code in safeList:
-				t = (int(end_time)-int(start_time)+1)/2 + int(start_time)
+				#t = (int(end_time)-int(start_time)+1)/2 + int(start_time)
+				t = (int(end_time)-1+1)/2 + 1
 				clk = code
 				s = "0%s"%clk
 				write_file.append(s)
@@ -171,33 +247,120 @@ def parse_vcd(vcd_file, start_time, end_time):
 				if 'count' in data[code]:
 					for i in range(len(data[code]['tv'])):			# counter
 						if int(data[code]['tv'][i][0])>=int(start_time)  and i>=1:
+							#print code
+							#print 'now:%s'%data[code]['tv'][i-1]
+							#print data[code]['tv'][i]
 							a = data[code]['tv'][i][1]
 							b = data[code]['tv'][i-1][1]
-							c = toggle_count(str(a),str(b))
-							data[code]['count'] += c
-							if int(data[code]['tv'][i][0])==int(end_time):
-								break
+							if str(a)!="x" and str(b)!="x":
+								c = toggle_count(str(a),str(b))
+								data[code]['count'] += c
+								if int(data[code]['tv'][i][0])==int(end_time):
+									break
 					binCount = count_length(data[code]['count'])	# invert count to binary type
 					s = "b%s %s"%(binCount, code)
 					write_file.append(s)
+
 		s = "#%s"%str(t)
 		write_file.append(s)
 		for code in safeList:
 			s = "1%s"%code
 			write_file.append(s)
-			
-		end_time = int(end_time) +1
+		end_time = int(end_time) + 1
 		write_file.append("#%s"%end_time)
-
+		
+		'''
+		for code in data.keys():
+			size = getActSize(int(data[code]['nets'][0]['size']))
+			count = int(data[code]['count'])
+			#print "size: %s, count: %s"%(size, count)
+			if count>size:
+				for i in write_file:
+					if i.startswith('$var'):
+						l = i.split()
+						if l[4]==data[code]['nets'][0]['name'] or l[3]==code:
+							idx = write_file.index(i)
+							c1 = l[3]
+							newCode = getNewCode(data)
+							#data[newCode]={}
+							l[3] = newCode
+							write_file[idx] = '$var %s\t  %s %s\t%s  $end'%(l[1],l[2],l[3],l[4])
+							newType = data[code]['nets'][0]['type']
+							newName = data[code]['nets'][0]['name'] + '_v2 [31:0]'
+							string = '$var %s\t  32 %s\t%s  $end'%(newType, c1, newName)
+							write_file.insert(idx+1, string)
+	
+		'''
+		i = 0
+		while(i<len(write_file)):
+			statement = write_file[i]
+			#print statement
+			if statement.startswith('$var'):
+				l = statement.split()
+				code = l[3]
+				#print code
+				size = getActSize(int(data[code]['nets'][0]['size']))
+				count = int(data[code]['count'])
+				if int(count)>int(size):
+					newCode = getNewCode(data)
+					data[newCode] = {'count':0, 'size':0}
+					write_file[i] = '$var %s\t  %s %s\t%s  $end'%(l[1],l[2],newCode,l[4])
+					newType = data[code]['nets'][0]['type']
+					newSize = data[code]['nets'][0]['size']
+					#print data[code]['nets'][0]['length']
+					if data[code]['nets'][0]['length'] != '1':
+						newRange = data[code]['nets'][0]['length']
+						newLength = re_len.match(newRange)
+						a = newLength.group()
+						a = a.replace('[','')
+						a = a.replace(']','')
+						b = a.split(':')
+						startBit = b[1]
+						endBit = b[0]
+						newName = data[code]['nets'][0]['shortName'] + '_%s__%s'%(endBit, startBit) + ' [%s:%s]'%(str(int(startBit)+32), startBit)
+					else:
+						newName = data[code]['nets'][0]['shortName'] + '_1__0 [31:0]'
+					string = '$var %s\t  32 %s\t%s  $end'%(newType, code, newName)
+					data[code]['replace'] = True
+					write_file.insert(i+1,string)
+					#print "code:%s"%code
+					i = i + 1
+			if statement.startswith('x'):
+				rep = re_init.match(statement)
+				copyCode = rep.group(1)
+				if data[copyCode]['replace']:
+					#print copyCode
+					write_file[i] = 'bxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx %s'%copyCode
+				
+				
+			i = i + 1
+	'''
+	test = open(csv, 'r')
+	while True:
+		ll = test.readline()
+		if ll=="":
+			break
+		ll.strip()
+		testword = ll.split(',\t\t')
+		#print "testword:\n%s"%testword
+		for code in data:
+			#print data[code]['nets'][0]['name']
+			if str(data[code]['nets'][0]['name']) == str(testword[2]) and str(data[code]['nets'][0]['hier'])==str(testword[1]):
+				if int(data[code]['count'])!=int(testword[3]):
+					print "%s not the same!"%code
+	'''
+	
 	fh.close()
 	return data, write_file
 
 def main():
 	vcd_file, start_time, end_time = sys.argv[1], sys.argv[2], sys.argv[3]
+	#csv = sys.argv[4]
 
+	#write_file = parse_vcd(vcd_file, start_time, end_time, csv)
 	vcd, write_file = parse_vcd(vcd_file, start_time, end_time)		# vcd = record all time's signals
-	for p in write_file:
-		print p
+	#for p in write_file:
+	#	print p
 	f = open('t1.vcd','w')
 
 	for each in write_file:
